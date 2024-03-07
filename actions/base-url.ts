610630232cf7cac099a41bf33e2064bb489578bd
Halibal <halibal95@gmail.com>
2024-03-08 01:26:40 +0300

    created the app

---
+++ actions/base-url.ts
@@ -0,0 +1,5 @@
+"use server";
+
+export const NEWS_API_BASE_URL = process.env.NEWS_API_BASE_URL;
+
+export const NY_TIMES_BASE_URL = process.env.NY_TIMES_BASE_URL;
---
+++ lib/functions/error-object.ts
@@ -0,0 +1,6 @@
+export const errorObject = (message: string) => {
+    return {
+        status: "error",
+        message,
+    };
+};
